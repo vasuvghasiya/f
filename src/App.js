1c6698468116718c64e4e02103b232add8893820
vasuvghasiya <160381198+vasuvghasiya@users.noreply.github.com>
2024-03-29 18:15:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//App.js
+
+import './App.css';
+import ChatRoom from './chatroom';
+
+function App() {
+	return (
+		<div className="App">
+			<ChatRoom />
+		</div>
+	);
+}
+
+export default App;
